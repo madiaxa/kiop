def38aab06b3b12f0d9e785aca84c227b4683024
madiaxa <madiaxa@proton.me>
2026-08-24 08:26:11 -0600

    Change welcome message and update logo links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,20 +12,13 @@
 </script>
 
 <main class="container">
-  <h1>Welcome to Tauri + Svelte</h1>
+  <h1>Welcome to Kiop</h1>
 
   <div class="row">
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
-    </a>
-    <a href="https://tauri.app" target="_blank">
-      <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank">
-      <img src="/svelte.svg" class="logo svelte-kit" alt="SvelteKit Logo" />
-    </a>
+    <a href="https://github.com/madiaxa/kiop" target="_blank">
+      <img src="/tauri.svg" class="logo tauri" alt="Kiop Logo" />
   </div>
-  <p>Click on the Tauri, Vite, and SvelteKit logos to learn more.</p>
+  <p>Click on the Kiop logo to learn more.</p>
 
   <form class="row" onsubmit={greet}>
     <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
